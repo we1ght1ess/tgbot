48ae86696125f6740d0214e8b2e8fd82e333c926
we1ght1ess <144522028+we1ght1ess@users.noreply.github.com>
2023-09-30 01:45:15 +0700

    y

--- tgbot/main.py
+++ tgbot/main.py
@@ -2,7 +2,7 @@
 import telebot
 import pandas as pd
 
-TOKEN = '6169338679:AAGOyjud6ZV3VU-2_tRmhyNC2wabez_OUVU'
+TOKEN = '6169338679:AAGOyjud6ZV3VU-2_tRmhyNC2wabez_OUVU' #@Stud212126_bot
 bot = telebot.TeleBot(TOKEN)
 
 list =[]
